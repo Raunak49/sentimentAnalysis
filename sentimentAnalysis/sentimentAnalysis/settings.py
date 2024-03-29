707b8ceb365da1ef22297d5f24c4eb39cd0e85bc
Raunak49 <1000015951@dit.edu.in>
2024-03-30 01:36:45 +0530

    static files

--- sentimentAnalysis/sentimentAnalysis/settings.py
+++ sentimentAnalysis/sentimentAnalysis/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
